0d80d0f4ad607b08a4960719383df926b23c641c
JasonK <jkimmerling@protonmail.com>
2023-11-23 09:26:20 -0600

    Added the env variable setup script - for dev use

---
+++ set_envs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DBUSER="postgres"
+export DBPASS="mysecretpassword"
+export DBHOST="172.17.0.2"
+export MONGODBUSER=""
+export MONDOGDBPASS=""
+export MONGODBHOST=""
+export REDISHOST="redis://172.17.0.3"
+export APIHOST="http://127.0.0.1:8008/"
+export KAFKAHOST=""
+export MQTTHOST=""
